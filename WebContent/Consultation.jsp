0a4b70e9ec2fd7e45bcedd89ed5c22942eccb00a
Mdidu <alexandre.meddas@gmail.com>
2021-03-29 16:02:16 +0200

    first commit

---
+++ WebContent/Consultation.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<jsp:useBean id="CompteBean" class="comptes.Compte" type="comptes.Compte" scope="session" />
+
+	<jsp:setProperty property="*" name="CompteBean" />
+	Le solde de <jsp:getProperty property="proprietaire" name="CompteBean"/> est 
+	<jsp:getProperty property="solde" name="CompteBean"/>€ !
+</body>
+</html>
---
+++ src/comptes/Compte.java
@@ -0,0 +1,32 @@
+package comptes;
+
+public class Compte {
+	private int numCompte;
+	private float solde;
+	private String proprietaire;
+
+	public int getNumCompte() {
+		return numCompte;
+	}
+
+	public void setNumCompte(int numCompte) {
+		this.numCompte = numCompte;
+	}
+
+	public float getSolde() {
+		return solde;
+	}
+
+	public void setSolde(float solde) {
+		this.solde = solde;
+	}
+
+	public String getProprietaire() {
+		return proprietaire;
+	}
+
+	public void setProprietaire(String proprietaire) {
+		this.proprietaire = proprietaire;
+	}
+
+}
